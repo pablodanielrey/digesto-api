ade08a9fd96d359633d6b44076032a58144f0aa5
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-15 23:46:49 -0300

    agrego un script para importar a la base todos los archivos de una carpeta

---
+++ scripts/importar_archivos.py
@@ -0,0 +1,56 @@
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+import logging
+import sys
+import os
+import json
+import uuid
+import base64
+import datetime
+
+from digesto.model.Utils import md5sum
+from digesto.model import obtener_session
+from digesto.model.entities.Digesto import Archivo
+
+if __name__ == '__main__':
+
+    path = sys.argv[1]
+    logging.debug(f'subiendo normas desde {path}')
+
+    archivos = []
+    for (dirname, dirs, files) in os.walk(path):
+        for f in files:
+            archivos.append({
+                'nombre': f,
+                'archivo': '/'.join([dirname, f])
+            })
+
+    with obtener_session() as session:
+        logging.info(f'{len(archivos)} archivos a importar en la base')
+        count = 0
+        for a in archivos:
+            count = count + 1
+            logging.info(f"importando {count} - {a['nombre']}")
+            with open(a['archivo'],'rb') as f:
+                nombre = a['nombre']
+                contenido = f.read()
+                md5s = md5sum(contenido)
+                b64c = base64.b64encode(contenido)
+
+                if session.query(Archivo).filter(Archivo.nombre == nombre, Archivo.hash_ == md5s).count() <= 0:
+                    a = Archivo()
+                    a.id = str(uuid.uuid4())
+                    a.created = datetime.datetime.utcnow()
+                    a.nombre = nombre
+                    a.hash_ = md5s
+                    a.contenido = b64c
+                    a.tipo = 'application/pdf' if '.pdf' in nombre else ''
+                    session.add(a)
+                    session.commit()
+                else:
+                    logging.info('ya existe asi que no se importa')
+        
+        
+        
