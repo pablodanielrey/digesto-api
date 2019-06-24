2691c813913936b0725e71b72b2c6862ec9553fe
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-06-24 16:52:03 -0300

    agregue un script para descargar todos los archivos de la base en una carpeta

---
+++ scripts/miracion/descargar_archivos_de_base.py
@@ -0,0 +1,41 @@
+"""
+    descarga todos los archivos de la base y los deja en una carpeta
+"""
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+import logging
+import sys
+import os
+import json
+import uuid
+import base64
+import datetime
+
+from sqlalchemy.orm import defer
+
+from digesto.model.Utils import md5sum
+from digesto.model import obtener_session
+from digesto.model.entities.Digesto import Archivo
+
+
+
+if __name__ == '__main__':
+
+    path = sys.argv[1]
+    logging.info(f"Descargando archivos dentro de la carpeta : {path}")
+
+    with obtener_session() as session:
+        try:
+            archivos = session.query(Archivo).options(defer('contenido')).all()
+            for archivo in archivos:
+                nombre = archivo.nombre
+                logging.info(f"escribiendo archivo {nombre}")
+                contenido = archivo.contenido
+                contenido_binario = base64.b64decode(contenido.encode())
+                path_completo = f"{path}/{nombre}"
+                with open(path_completo, 'wb') as f:
+                    f.write(contenido_binario)
+
+        except Exception as e:
+            logging.exception(e)
