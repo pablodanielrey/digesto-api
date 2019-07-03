ae13ae4ba6e1193b80a05406ad789a58dd8ee748
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-07-03 12:58:43 -0300

    cambie la version que me habia olvidado

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='digesto-api',
-          version='1.0.2',
+          version='1.0.3',
           description='Proyecto que implementa la api del digesto institucional',
           url='https://github.com/pablodanielrey/digesto-api',
           author='Desarrollo DiTeSi, FCE',
