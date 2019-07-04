b09b34e4344fb8d456f242f3b0928f14f920fb97
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-07-04 14:10:32 -0300

    cambio la version de la api asi se que se modifico el comportamiento

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages
 
 setup(name='digesto-api',
-          version='1.0.5',
+          version='1.0.6',
           description='Proyecto que implementa la api del digesto institucional',
           url='https://github.com/pablodanielrey/digesto-api',
           author='Desarrollo DiTeSi, FCE',
