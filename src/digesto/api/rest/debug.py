d2b5ae32ed3f1a918d0bbfeeec185386fa0b0032
Pablo Daniel Rey <pablodanielrey@gmail.com>
2019-07-04 13:02:57 -0300

    cambie la sintaxis a ver si resuelve un problema con el path

--- src/digesto/api/rest/debug.py
+++ src/digesto/api/rest/debug.py
@@ -2,5 +2,5 @@ import ptvsd
 ptvsd.enable_attach(address=('0.0.0.0', 11304))
 ptvsd.wait_for_attach()
 
-from .wsgi import app
+from digesto.api.rest.wsgi import app
 app.run(host='0.0.0.0', port='11302', debug=False)
